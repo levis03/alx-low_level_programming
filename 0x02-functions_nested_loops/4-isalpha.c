959f708ef4ba9768359483c35f7b3470d1b29cf0
Levis <levismaina03@gmail.com>
2022-10-19 03:10:30 -0700

    New files

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * -isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && <= 'z') || (c >= 'A' && c <= 'Z'));
+
+	return(0);
+}
