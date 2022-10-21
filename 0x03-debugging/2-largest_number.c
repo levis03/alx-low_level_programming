b841c4abc6c6dac6f00f323365b5a9cb5826927b
Levis <levismaina03@gmail.com>
2022-10-21 13:40:18 -0700

    Git amends

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,16 +10,19 @@
 
 int largest_number(int a, int b, int c)
 {
-		int largest = b;
+		int largest;
 
-			if (a > b)
-					{
-								largest = a;
-									}
-				else if (c > b)
-						{
-									largest = c;
-										}
-
-					return (largest);
+		if(a >= b && a >= c)
+		{
+			largest = a;
+		}
+		else if (b >= a && b >=c)
+		{
+			largest = b;
+		}
+		else
+		{
+			largest = c;
+		}
+		return(largest);
 }
