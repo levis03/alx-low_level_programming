88b9393e51890b049607e520490c9bb35eb48d9c
Levis Maina <88320351+levis03@users.noreply.github.com>
2022-11-10 22:40:13 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,21 +14,21 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 		void *mem;
-			char *filler;
-				unsigned int index;
+		char *filler;
+		unsigned int index;
 
-					if (nmemb == 0 || size == 0)
-								return (NULL);
+		if (nmemb == 0 || size == 0)
+		return (NULL);
 
-						mem = malloc(size * nmemb);
+		mem = malloc(size * nmemb);
 
-							if (mem == NULL)
-										return (NULL);
+		if (mem == NULL)
+		return (NULL);
 
-								filler = mem;
+		filler = mem;
 
-									for (index = 0; index < (size * nmemb); index++)
+		for (index = 0; index < (size * nmemb); index++)
 												filler[index] = '\0';
 
-										return (mem);
+		return (mem);
 }
